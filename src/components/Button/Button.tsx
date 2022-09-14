da65f0d31deee778fbe68ac28ea4367df6d76f94
yoon <yunyun40550082@gmail.com>
2022-09-14 15:41:13 +0900

    refactor: button컴포넌트 - 스타일드 컴포넌트에서 함수 컴포넌트로 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,20 @@
 import styled from '@emotion/styled'
 
-type ButtonProps = React.ComponentProps<'button'>
+interface ButtonProps extends React.ComponentProps<'button'> {
+  round: boolean
+}
 
-const Button = styled.button<ButtonProps>`
+const Button = ({ round = false, onClick, ...props }) => {
+  return <StyledButton onClick={onClick} round={round} {...props} />
+}
+
+const StyledButton = styled.button<ButtonProps>`
   width: 100%;
   max-width: 500px;
   height: 40px;
   font-size: 14px;
   font-weight: 400;
-  border-radius: 2px;
+  border-radius: ${({ round }) => (round ? '100px' : '2px')};
   transition: all 150ms ease-out;
 `
 
