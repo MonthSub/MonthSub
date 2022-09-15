fa222ec26f7d48008caad0509055e7084468dc77
yoon <yunyun40550082@gmail.com>
2022-09-15 13:20:37 +0900

    refactor: css props으로 theme props를 한번만 꺼내서 사용하도록 간결하게 수정

--- src/components/atom/Button/LineButton.tsx
+++ src/components/atom/Button/LineButton.tsx
@@ -1,13 +1,16 @@
 import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 import Button from './Button'
 
 const LineButton = styled(Button)`
-  border: ${({ theme }) => `1px solid ${theme.color.border}`};
-  color: ${({ theme }) => theme.color.gray_2};
-  &:hover {
-    border-color: ${({ theme }) => theme.color.main_coral};
-    color: ${({ theme }) => theme.color.main_coral};
-  }
+  ${({ theme }) => css`
+    border: 1px solid ${theme.color.border};
+    color: ${theme.color.gray_2};
+    &:hover {
+      border-color: ${theme.color.main_coral};
+      color: ${theme.color.main_coral};
+    }
+  `}
 `
 
 export default LineButton
