d5d6f78de2e89aed3f207fdbe4cccec8d6035860
SONG YI PARK <69751205+thddlmy@users.noreply.github.com>
2022-09-21 22:56:16 +0900

    feat: Tag 컴포넌트 구현
    * feat: Tag 컴포넌트 구현
    
    * refactor: 태그 이름 변경

---
+++ src/components/atom/Tag/GrayTag.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+import Tag from './Tag'
+
+const GrayTag = styled(Tag)`
+  ${({ theme, reverse }) => css`
+    border: 1px solid ${theme.color.gray_3};
+    color: ${reverse ? theme.color.gray_3 : '#fff'};
+    background-color: ${reverse ? '#fff' : theme.color.gray_3};
+  `}
+`
+
+export default GrayTag
---
+++ src/components/atom/Tag/Tag.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+import { reverse } from 'dns'
+
+interface TagProps extends React.ComponentProps<'div'> {
+  reverse: boolean
+}
+
+const Tag = ({ children, reverse, ...props }) => {
+  return (
+    <TagBox reverse={reverse} {...props}>
+      {children}
+    </TagBox>
+  )
+}
+
+const TagBox = styled.div<TagProps>`
+  width: 48px;
+  height: 24px;
+  font-size: 12px;
+  padding: 4px;
+  border-radius: 12px;
+  text-align: center;
+  line-height: 1.5;
+`
+
+export default Tag
